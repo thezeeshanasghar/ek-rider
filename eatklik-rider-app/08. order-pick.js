480fa8d9d78bd9da0384640b6cdfb0eba686bfc9
Naeem_Abbas <naeemabbasa25@gmail.com>
2020-04-28 08:50:38 +0500

    Map Changes

--- eatklik-rider-app/08. order-pick.js
+++ eatklik-rider-app/08. order-pick.js
@@ -95,7 +95,87 @@ longitude=position.coords.longitude;
     
     var mapOptions = {
       zoom: 7,
-      center: new google.maps.LatLng(30.3753, 69.3451)
+      center: new google.maps.LatLng(30.3753, 69.3451),
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ]
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     directionsDisplay.setMap(map);
@@ -108,9 +188,13 @@ longitude=position.coords.longitude;
    
     //var end = new google.maps.LatLng(38.334818, -181.884886);
     var end = new google.maps.LatLng(rest_latitude, rest_longitude);
-    var StartingPoint = new google.maps.Marker({icon: "img/starting_point.ico", map: map, position: start});
-    var Rest = new google.maps.Marker({icon: "img/rest.ico", map: map, position: end});
-    var Customer = new google.maps.Marker({icon: "img/user.ico", map: map, position: new google.maps.LatLng(cust_latitude,cust_longitude)});
+    var StartingPoint = new google.maps.Marker(
+      {
+        icon: "img/clipart-arrow-grey-16.ico",
+      label: { color: '#00aaff', fontWeight: 'bold', fontSize: '14px', text: 'Your Starting Point' },
+      map: map, position: start});
+    var Rest = new google.maps.Marker({icon: "img/restaurant_location .ico", map: map, position: end});
+    var Customer = new google.maps.Marker({icon: "img/Customer_location.ico", map: map, position: new google.maps.LatLng(cust_latitude,cust_longitude)});
     var request = {
       origin: new google.maps.LatLng(rest_latitude, rest_longitude),
       destination: new google.maps.LatLng(cust_latitude,cust_longitude),
